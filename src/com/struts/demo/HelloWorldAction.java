503dabcf4ef33ee9043f65e0204b0c9c1811183e
admin <569329637@qq.com>
2017-09-03 01:09:41 +0800

    init

---
+++ src/com/struts/demo/HelloWorldAction.java
@@ -0,0 +1,20 @@
+package com.struts.demo;
+
+public class HelloWorldAction {
+	private String name;
+	
+	public String execute() throws Exception {
+		
+		System.out.println("fdfd");
+		return "success";
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
